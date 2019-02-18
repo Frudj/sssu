06e90d4e43455733791326c3322ee1b214d948d6
Frudj <soloblud@gmail.com>
2019-02-18 23:10:07 +0300

    Create

---
+++ lab1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите a");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double z1 = (Math.Sin(2 * a) + Math.Sin(5 * a) - Math.Sin(3 * a)) / (Math.Cos(a) - Math.Cos(3 * a) + Math.Cos(5 * a));
+            double z2 = Math.Tan(3 * a);
+            Console.WriteLine("Результат =" + z1);
+            Console.WriteLine("Результат =" + z2);
+        }
+    }
+}
